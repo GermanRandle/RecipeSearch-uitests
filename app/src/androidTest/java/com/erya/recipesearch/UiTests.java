c0d1f46817b0379f60662fdc9a91635c81865014
German Andosov <334875@niuitmo.ru>
2022-10-05 23:49:32 +0300

    third test, found a bug

--- app/src/androidTest/java/com/erya/recipesearch/UiTests.java
+++ app/src/androidTest/java/com/erya/recipesearch/UiTests.java
@@ -36,35 +36,52 @@ import java.util.Random;
 @RunWith(AndroidJUnit4.class)
 public class UiTests {
 
-    // For how long we wait for Spoonacular API to react and views to create
-    private static final int WAIT_TIME = 2000;
+    private static final String LOG_TAG = "UI_TEST";
 
     @Rule
     public ActivityTestRule<MainActivity> testMainActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
-    public void deleteAllSavedIngredients() throws Exception {
-
-        // TODO: delete all favorite recipes too
+    public void deleteAllSavedInfo() {
 
         clickMainButton();
 
         // Need to wait until all photos are loaded and RecyclerView items are created
-        Thread.sleep(WAIT_TIME);
+        waitForApi();
 
-        // Delete all saved ingredients
-        RecyclerView ingredients = testMainActivityRule.getActivity()
+        // Delete all saved products
+        RecyclerView products = testMainActivityRule.getActivity()
                 .findViewById(com.project.giniatovia.feature_fridge.R.id.rv);
-        var needToDelete = Objects.requireNonNull(ingredients.getAdapter()).getItemCount();
-        for (int index = needToDelete - 1; index >= 0; index--) {
-            onView(withIndex(withId(com.project.giniatovia.feature_fridge.R.id.image_close), index)).perform(click());
+        int productsToDelete = Objects.requireNonNull(products.getAdapter()).getItemCount();
+        for (int index = productsToDelete - 1; index >= 0; index--) {
+            onView(withIndex(withId(com.project.giniatovia.feature_fridge.R.id.image_close),
+                            index)).perform(click());
         }
 
+        // Back to start screen
+        previousScreen();
+
+        // Delete all saved recipes
+        onView(withId(R.id.secondary_btn))
+                .perform(click());
+        RecyclerView savedRecipes = testMainActivityRule.getActivity()
+                .findViewById(com.project.giniatovia.feature_recipe.R.id.rv_recipe);
+        int favoritesCount = Objects.requireNonNull(savedRecipes.getAdapter()).getItemCount();
+        for (int index = favoritesCount - 1; index >= 0; index--) {
+            onView(withIndex(withId(com.project.giniatovia.feature_fridge.R.id.product_image),
+                            index)).perform(click());
+            waitForApi();
+            onView(withId(com.project.giniatovia.feature_recipe.R.id.bookmark))
+                    .perform(click());
+            previousScreen();
+        }
+
+        // Back to start screen
         previousScreen();
     }
 
     @Test
-    public void cantShowRecipesIfNoIngredients() throws Exception {
+    public void cantShowRecipesIfNoProducts() {
 
         clickMainButton();
 
@@ -72,12 +89,12 @@ public class UiTests {
         onView(withId(com.project.giniatovia.feature_fridge.R.id.main_btn))
                 .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
 
-        addIngredient("Potatoes");
+        addProduct("Potatoes");
 
-        // Wait a little
-        Thread.sleep(WAIT_TIME);
+        // Wait for product to load
+        waitForApi();
 
-        // Remove ingredient
+        // Remove product
         onView(withId(com.project.giniatovia.feature_fridge.R.id.image_close))
                 .perform(click());
 
@@ -87,39 +104,11 @@ public class UiTests {
     }
 
     @Test
-    public void addRecipeToFavoritesThenRemove() throws Exception {
+    public void saveRecipeCheckSaveThenRemove() {
 
-        clickMainButton();
+        addSomeRecipeToFavorites();
 
-        // Add some ingredients
-        addIngredient("Apple");
-        addIngredient("Orange");
-        addIngredient("Banana");
-
-        // Show recipes and wait for RecyclerView to appear
-        showRecipes();
-        var recyclerView = onView(withId(com.project.giniatovia.feature_recipe.R.id.rv_recipe));
-        RecyclerView recipes = testMainActivityRule.getActivity()
-                .findViewById(com.project.giniatovia.feature_recipe.R.id.rv_recipe);
-        Thread.sleep(WAIT_TIME);
-
-        // Choose random recipe
-        var recipesFound = Objects.requireNonNull(recipes.getAdapter()).getItemCount();
-        Assert.assertTrue(recipesFound > 0);
-        // TODO: support RecyclerView scrolling
-        var randomPos = new Random().nextInt(Math.min(recipesFound, 4));
-        Log.i("TEST", "addRecipeToFavorites: random recipe position: " + randomPos);
-
-        // Click on recipe and wait for recipe to appear
-        recyclerView.perform(actionOnItemAtPosition(randomPos, click()));
-        Thread.sleep(WAIT_TIME);
-
-        // Add to favorites
-        // TODO: check that bookmark icon changed color
-        onView(withId(com.project.giniatovia.feature_recipe.R.id.bookmark))
-                .perform(click());
-
-        // Return to start screen
+        // Back to start screen
         previousScreen();
         previousScreen();
         previousScreen();
@@ -135,7 +124,7 @@ public class UiTests {
         Assert.assertEquals(1, favoritesCount);
         onView(withId(com.project.giniatovia.feature_recipe.R.id.product_image))
                 .perform(click());
-        Thread.sleep(WAIT_TIME);
+        waitForApi();
         onView(withId(com.project.giniatovia.feature_recipe.R.id.bookmark))
                 .perform(click());
 
@@ -144,6 +133,33 @@ public class UiTests {
         savedRecipes = testMainActivityRule.getActivity()
                 .findViewById(com.project.giniatovia.feature_recipe.R.id.rv_recipe);
         favoritesCount = Objects.requireNonNull(savedRecipes.getAdapter()).getItemCount();
+        Assert.assertEquals(0, favoritesCount);
+    }
+
+    @Test
+    public void addToFavoritesThenImmediatelyRemove() {
+
+        /*
+         This test should fail because it's a bug in our application :)
+         */
+
+        addSomeRecipeToFavorites();
+
+        // Remove from favorites
+        onView(withId(com.project.giniatovia.feature_recipe.R.id.bookmark))
+                .perform(click());
+
+        // Return to start screen
+        previousScreen();
+        previousScreen();
+        previousScreen();
+
+        // Check whether there are no saved recipes
+        onView(withId(R.id.secondary_btn))
+                .perform(click());
+        RecyclerView savedRecipes = testMainActivityRule.getActivity()
+                .findViewById(com.project.giniatovia.feature_recipe.R.id.rv_recipe);
+        int favoritesCount = Objects.requireNonNull(savedRecipes.getAdapter()).getItemCount();
         Assert.assertEquals(0, favoritesCount);
     }
 
@@ -165,6 +181,39 @@ public class UiTests {
         };
     }
 
+    private void addSomeRecipeToFavorites() {
+
+        clickMainButton();
+
+        // Add some products
+        addProduct("Apple");
+        addProduct("Orange");
+        addProduct("Banana");
+
+        // Show recipes and wait for RecyclerView to appear
+        showRecipes();
+        var recyclerView = onView(withId(com.project.giniatovia.feature_recipe.R.id.rv_recipe));
+        RecyclerView recipes = testMainActivityRule.getActivity()
+                .findViewById(com.project.giniatovia.feature_recipe.R.id.rv_recipe);
+        waitForApi();
+
+        // Choose random recipe
+        var recipesFound = Objects.requireNonNull(recipes.getAdapter()).getItemCount();
+        Assert.assertTrue(recipesFound > 0);
+        // TODO: support RecyclerView scrolling
+        var randomPos = new Random().nextInt(Math.min(recipesFound, 4));
+        Log.i(LOG_TAG, "addRecipeToFavorites: random recipe position: " + randomPos);
+
+        // Click on recipe and wait for recipe to appear
+        recyclerView.perform(actionOnItemAtPosition(randomPos, click()));
+        waitForApi();
+
+        // Add to favorites
+        // TODO: check that bookmark icon changed color
+        onView(withId(com.project.giniatovia.feature_recipe.R.id.bookmark))
+                .perform(click());
+    }
+
     private void clickMainButton() {
 
         // Clicking "Choose a new dish"
@@ -172,20 +221,15 @@ public class UiTests {
                 .perform(click());
     }
 
-    private void typeIngredient(String text) {
+    private void addProduct(String product) {
 
-        // Type ingredient in AutoCompleteTextView
+        // Type product in AutoCompleteTextView
         onView(withId(com.project.giniatovia.feature_fridge.R.id.autoCompleteTextView))
                 .perform(click())
-                .perform(typeText(text));
-    }
+                .perform(typeText(product));
 
-    private void addIngredient(String ingredient) {
-
-        typeIngredient(ingredient);
-
-        // Add ingredient
-        onView(withText(ingredient))
+        // Add product
+        onView(withText(product))
                 .inRoot(RootMatchers.isPlatformPopup())
                 .perform(click());
     }
@@ -202,5 +246,13 @@ public class UiTests {
         // TODO: I know checking contentDescription like that is not good but
         //  on some screens arrow is added in Kotlin code and thus don't appear in R.id
         onView(withContentDescription("Go back")).perform(click());
+    }
+
+    private void waitForApi() {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            Log.e(LOG_TAG, "Thread.sleep() was interrupted: " + e.getMessage());
+        }
     }
 }
